b689cee32dce6ed62403d6dd8276f3436b039df2
jobayercse71 <jobayercse71.com>
2021-09-02 11:32:25 +0600

    test

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
+// search book
+
 const searchBook = () => {
 
     const input = document.getElementById('bookInput');
     const inputValue = input.value;
+    if(inputValue===''){
 
-    const url = `http://openlibrary.org/search.json?q=${inputValue}`;
+    }
+    else{
+        const url = `http://openlibrary.org/search.json?q=${inputValue}`;
 
-    fetch(url)
+        fetch(url)
         .then(res => res.json())
         .then(data => displayData(data, data.docs));
+    }
+    
 
     input.value = '';
 
-
-
 }
+
+
+// display book
 
 const displayData = (data, book) => {
     document.getElementById('bookNumber').innerText = data.numFound;
     console.log(data.numFound);
     console.log(book);
+
     const bookSection = document.getElementById('book-section');
     const notFound = document.getElementById('notFound');
 
@@ -31,24 +40,23 @@ const displayData = (data, book) => {
 
         bookSection.textContent = '';
         book.forEach(element => {
-            console.log(element);
+        console.log(element);
 
-            const div = document.createElement('div');
+        const div = document.createElement('div');
 
-            div.classList.add('col');
-            div.innerHTML = `
+        div.classList.add('col');
+        div.innerHTML = `
         <div class="card">
         <img height="200px" src="https://covers.openlibrary.org/b/id/${element.cover_i}-M.jpg" class="card-img-top" alt="...">
         <div class="card-body">
           <h5 class="card-title">${element.title}</h5>
           <p class="card-text">Author Name: ${element.author_name}</p>
           <p class="card-text">Publisher: ${element.publisher}</p>
-          <p class="card-text">Publish Date: ${element.publish_date}</p>
+          <p class="card-text">Publish Date: ${element.first_publish_year}</p>
         </div>
         </div>
         `;
-            bookSection.appendChild(div);
-
+        bookSection.appendChild(div);
 
         });
 
